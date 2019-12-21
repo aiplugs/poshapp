e72fe3127386da46e8c700df862681fe7af08d78
iwate <iwatesoftware@live.jp>
2019-12-21 18:38:32 +0900

    remove unused file

--- src/Aiplugs.PoshApp/wwwroot/js/fsm.js
+++
@@ -1,6 +0,0 @@
-class FSM
-{
-    constructor() {
-        this.state = 'state'
-    }
-}
--- src/Aiplugs.PoshApp/wwwroot/js/import.js
+++
@@ -1,6 +0,0 @@
-const links = document.querySelectorAll('link[rel="import"]')
-
-Array.prototype.forEach.call(links, function (link) {
-  let template = link.import.querySelector('template')
-  document.querySelector('body').appendChild(template)
-})
