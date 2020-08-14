33755e1225d1da91425fe284da72e036d50f24e0
iwate <iwatesoftware@live.jp>
2020-08-14 15:09:12 +0900

    init electron proj

---
+++ src/Aiplugs.PoshApp.Electron/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>POSH App {{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello, World!"
+        }
+    }
+}
+</script>
