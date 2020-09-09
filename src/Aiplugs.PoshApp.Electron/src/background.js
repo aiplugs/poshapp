d9df24ae364ac822983d6bde18362d55bb46ae40
Yoshiyuki <iwate@tamainozominoMacBook-Pro.local>
2020-09-09 11:52:42 +0900

    modify deamon path

--- src/Aiplugs.PoshApp.Electron/src/background.js
+++ src/Aiplugs.PoshApp.Electron/src/background.js
@@ -191,8 +191,8 @@ if (isDevelopment) {
 let connection;
 async function startPowerShellDeamon () {
   const binary = process.platform == 'win32' ? 'Aiplugs.PoshApp.Deamon.exe' : 'Aiplugs.PoshApp.Deamon'
-  const prefix = process.platform == 'darwin' ? 'Resources/bin' : 'resources/bin'
-  const deamon = isDevelopment ? `bin/${binary}` : `${prefix}/${binary}`;
+  const prefix = process.platform == 'darwin' ? '../Resources/bin' : 'resources/bin'
+  const deamon = isDevelopment ? `bin/${binary}` : path.join(__dirname, prefix, binary)
   const childProcess = cp.spawn(deamon);
 
   // Use stdin and stdout for communication:
