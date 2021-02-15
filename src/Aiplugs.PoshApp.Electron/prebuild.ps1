f64e82d6971eed5c27b84efc05b35d1810bb850f
iwate <iwatesoftware@live.jp>
2021-02-15 16:36:11 +0900

    modify prebuild

--- src/Aiplugs.PoshApp.Electron/prebuild.ps1
+++ src/Aiplugs.PoshApp.Electron/prebuild.ps1
@@ -14,7 +14,11 @@ if (-not ($csproj | ?{$_ -eq "<NoWarn>NU1605</NoWarn>"})) {
     $csproj.Replace("<AssemblyTitle>PowerShell Editor Services</AssemblyTitle>","<AssemblyTitle>PowerShell Editor Services</AssemblyTitle>`n<NoWarn>NU1605</NoWarn>") | Out-File -Path $lib
 }
 dotnet publish ../Aiplugs.PoshApp.Deamon -c Release -r $rid
-dotnet publish ../Aiplugs.PoshApp.Pses -c Release -r $rid
+if ($IsWindows) {
+    dotnet publish ../Aiplugs.PoshApp.Pses -c Release 
+} else {
+    dotnet publish ../Aiplugs.PoshApp.Pses -c Release -r $rid
+}
 
 if (-not (Test-Path -Path ./bin)) {
     mkdir ./bin
@@ -26,7 +30,11 @@ mkdir ./bin/pses/
 mkdir ./bin/pses/bin/
 mkdir ./bin/pses/bin/Common/
 Copy-Item -Recurse -Force -Path ../Aiplugs.PoshApp.Deamon/bin/Release/net5.0/$rid/publish/* -Destination ./bin/deamon/
-Copy-Item -Recurse -Force -Path ../Aiplugs.PoshApp.Pses/bin/Release/net5.0/$rid/publish/* -Destination ./bin/pses/bin/Common/
+if ($IsWindows) {
+    Copy-Item -Recurse -Force -Path ../Aiplugs.PoshApp.Pses/bin/Release/net5.0/publish/* -Destination ./bin/pses/bin/Common/
+} else {
+    Copy-Item -Recurse -Force -Path ../Aiplugs.PoshApp.Pses/bin/Release/net5.0/$rid/publish/* -Destination ./bin/pses/bin/Common/
+}
 if (-not (Test-Path -Path ../PSScriptAnalyzer.zip)) {
     Invoke-RestMethod https://github.com/PowerShell/PSScriptAnalyzer/releases/download/1.19.1/PSScriptAnalyzer.zip -OutFile ../PSScriptAnalyzer.zip
 }
